495a30c77b8271f2c2964911cd3766a4e592d264
yuudai-watanabe <74393413+yuudai-watanabe@users.noreply.github.com>
2020-11-13 13:28:41 +0900

    Create index.php

---
+++ dist/index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.typekit.net/yvb3frz.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header id="header">
+        <div class="inner">
+            <ul class="header-list">
+                <li>
+                    <a class="header-link" href="#top">
+                        <p class="header-link-jp">トップ</p>
+                        <p class="header-link-en">TOP</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="header-link" href="#news">
+                        <p class="header-link-jp">ニュース</p>
+                        <p class="header-link-en">NEWS</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="header-link" href="#story">
+                        <p class="header-link-jp">ストーリー</p>
+                        <p class="header-link-en">STORY</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="header-link" href="#comments">
+                        <p class="header-link-jp">著名人コメント</p>
+                        <p class="header-link-en">COMMENTS</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="header-link" href="#cast">
+                        <p class="header-link-jp">キャスト</p>
+                        <p class="header-link-en">CAST</p>
+                    </a>
+                </li>
+                <li>
+                    <a class="header-link" href="#inquiry">
+                        <p class="header-link-jp">問い合わせ</p>
+                        <p class="header-link-en">INQUIRY</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <section id="top">
+        <div class="top-container">
+            <div class="top-heading">
+                <h1><img src="./img/top-heading.png" alt="世界最長の叙事詩をピーター・ブルック以来の全編舞台化完全版マハーバーラタ"></h1>
+            </div>
+            <div class="top-wrapper">
+                <div class="top-wrapper-left">
+                    <h2><img src="./img/top-nakano.png" alt="2020年7月4日〜7日なかのZERO大ホール"></h2>
+                </div>
+
+                <div class="top-wrapper-right">
+                    <p>小池博史ブリッジプロジェクトが2013年から2020年までの8カ年計画で臨む、インド古代叙事詩「マハーバーラタ」の全編舞台作品化計画。いよいよその集大成である<span>完全版マハーバーラタ</span>の公演が決定！！
+                    </p>
+                    <p>アジア各国のアーティストらが共同で取り組み、発展を遂げる本事業はアジア、そして世界へ向けてビッグウェーブを起こす。タイと日本から世界へと発信する本作。どうぞお見逃しなく！</p>
+                </div>
+            </div>
+            <div class="btn top-btn">
+                <a href="#">チケット予約サイトへ</a>
+            </div>
+        </div>
+    </section><!-- /#top -->
+
+    <section id="news">
+        <div class="news-container inner">
+            <div class="intro-wrapper intro-img">
+                <h2 class="intro-heading">INTRODUCTION</h2>
+                <h3 class="intro-why">なぜ今「マハーバーラタ」なのか？</h3>
+                <div class="intro-textarea">
+                    <p class="intro-text">「平和」について改めて考えるストーリー全世界を司るものが神だとすれば、なぜ神であるクリシュナは、策を巡らしてまで、登場人物すべてを滅亡へと導いたのか？<br>それは、「戦い」そのものを廃絶しようとしたからだと考えられる。戦うことそのものへの虚しさ、「戦い」そのものの「悪」を問う物語が「マハーバーラタ」と言える。<br>平和とは？私たちには何ができるのか？根源的な「平和」を希求する物語。</p>
+                    <p class="intro-text">
+                    現代社会を映し出す人間ドラマ対難民問題やヘイトスピーチ問題に見られるように、文化的背景が「異」なるものに対して非寛容になりつつある現代社会。<br>神の血を引きながらも、現代人同様に欲望や嫉妬によって争う登場人物たちが破滅していく様を描いた「マハーバーラタ」のストーリーは、人類が歩んできた争いの歴史の物語とも言い換えられる。<br>非寛容による悲劇の物語である「マハーバーラタ」を現代社会に重ね合わせつつ描くことで「寛容」の重要性を示す。
+                    </p>
+                </div>
+            </div>
+            
+            <div class="news-wrapper">
+                <h2 class="news-heading">NEWS</h2>
+                <div class="btn news-btn">
+                    <a href="#">ニュース一覧へ</a>
+                </div>
+                                
+                <ul class="news-cards">
+                    <li class="news-card-top">
+                        <img src="./img/news-img1.png" alt="ニュース画像">
+                        <div class="news-textarea">
+                            <p class="news-text"><time datetime=”2019-9-30” itemprop=”datepublished”>
+                                2019.9.30
+                            </time> 「完全版マハーバーラタ」2020年7月・東京公演！！</p>
+                        </div>
+                    </li>
+                    <li class="news-card-top">
+                        <img src="./img/news-img2.png" alt="ニュース画像">
+                        <div class="news-textarea">
+                            <p class="news-text"><time datetime=”2019-9-30” itemprop=”datepublished”>
+                                2019.9.30
+                            </time> 「完全版マハーバーラタ」2020年7月・東京公演！！</p>
+                        </div>
+                    </li>
+                    <li class="news-card-top">
+                        <img src="./img/news-img3.png" alt="ニュース画像">
+                        <div class="news-textarea">
+                            <p class="news-text"><time datetime=”2019-9-30” itemprop=”datepublished”>
+                                2019.9.30
+                            </time> 「完全版マハーバーラタ」2020年7月・東京公演！！</p>
+                        </div>
+                    </li>
+                    <li class="news-card-bottom">
+                        <img src="./img/news-img2.png" alt="ニュース画像">
+                        <div class="news-textarea">
+                            <p class="news-text"><time datetime=”2019-9-30” itemprop=”datepublished”>
+                                2019.9.30
+                            </time> 「完全版マハーバーラタ」2020年7月・東京公演！！</p>
+                        </div>
+                    </li>
+                    <li class="news-card-bottom">
+                        <img src="./img/news-img2.png" alt="ニュース画像">
+                        <div class="news-textarea">
+                            <p class="news-text"><time datetime=”2019-9-30” itemprop=”datepublished”>
+                                2019.9.30
+                            </time> 「完全版マハーバーラタ」2020年7月・東京公演！！</p>
+                        </div>
+                    </li>
+                </ul>
+                
+            </div>
+            
+        </div>
+    </section><!-- /#news -->
+
+</body>
+
+</html>
